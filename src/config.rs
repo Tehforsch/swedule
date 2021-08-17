2389a46f76a41a73e04246af93dab541108e3cb0
tehforsch <mail@tonipeter.de>
2021-08-17 13:20:43 +0200

    use bwfor data

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub static SEND_TIME_OFFSET_SECONDS: f64 = 2e-6;
-pub static SEND_TIME_PER_BYTE_SECONDS: f64 = 9e-10;
+pub static SEND_TIME_OFFSET_SECONDS: f64 = 4e-6;
+pub static SEND_TIME_PER_BYTE_SECONDS: f64 = 1.8e-9;
 pub static SIZE_PER_MESSAGE: f64 = 32.0 * 2.0 + 64.0 * 5.0;
-pub static SOLVE_TIME: f64 = 1.25e-5;
+pub static SOLVE_TIME: f64 = 1.517e-5;
