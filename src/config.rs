bd4b857ee8feeb6bed493d9fcc8837a5cd2f60dd
tehforsch <mail@tonipeter.de>
2021-08-19 12:21:27 +0200

    remove comment

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,3 @@
-// pub static SEND_TIME_OFFSET_SECONDS: f64 = 4e-6;
-// pub static SEND_TIME_PER_BYTE_SECONDS: f64 = 1.8e-9;
 pub static SEND_TIME_OFFSET_SECONDS: f64 = 2e-6;
 pub static SEND_TIME_PER_BYTE_SECONDS: f64 = 1.8e-9;
 pub static SIZE_PER_MESSAGE: f64 = 32.0 * 2.0 + 64.0 * 5.0;
