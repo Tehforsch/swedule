f125aec167a66c2e50987b468812cb9cee2f8a91
tehforsch <Tonipeter92@googlemail.com>
2021-10-01 15:42:54 +0200

    fix merge

--- src/config.rs
+++ src/config.rs
@@ -1,20 +1,10 @@
-// // Bwfor
-// pub static SEND_TIME_OFFSET_SECONDS: f64 = 2e-6;
-// pub static SEND_TIME_PER_BYTE_SECONDS: f64 = 1.8e-9;
-// pub static SOLVE_TIME: f64 = 1.517e-5;
-
-// No comm
-pub static SEND_TIME_OFFSET_SECONDS: f64 = 0.0;
-pub static SEND_TIME_PER_BYTE_SECONDS: f64 = 0.0;
-pub static SOLVE_TIME: f64 = 1.0;
 pub static BATCH_SIZE: usize = 1;
 
-// // Empirical values
-// pub static SEND_TIME_OFFSET_SECONDS: f64 = 5e-4;
-// pub static SEND_TIME_PER_BYTE_SECONDS: f64 = 1.8e-9;
-
+// Empirical values
+pub static SEND_TIME_OFFSET_SECONDS: f64 = 5e-4;
+pub static SEND_TIME_PER_BYTE_SECONDS: f64 = 1.8e-9;
 pub static SIZE_PER_MESSAGE: f64 = 32.0 * 2.0 + 64.0 * 5.0;
-// pub static SOLVE_TIME: f64 = 1.517e-5;
+pub static SOLVE_TIME: f64 = 1.517e-5;
 pub static NUM_DIRECTIONS: usize = 84;
 
 pub const DIRECTION_BINS_84: [[f64; 3]; 84] = [
